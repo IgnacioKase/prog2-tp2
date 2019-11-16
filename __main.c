d8d0feacad82c1f801b438500ec1d3fd2d260673
Ignacio Kasevich <ignacio@kasevich.com.ar>
2019-11-16 15:45:18 -0300

    .

--- __main.c
+++ __main.c
@@ -5,22 +5,25 @@
 #define forr(i, a, b) for (int i = a; i < b; i++)
 #define forn(i, n) forr(i, 0, n)
 #define MAX_CITIES 2134
-#define MAX_PEOPLE 10000
+#define MAX_PEOPLE 100000
 #define MAX_STR_LENGTH 200
 #define PATH_CITIES "db/codigoLocalidades.txt"
 #define PATH_PEOPLE "db/personas.txt"
+#define PATH_OUT "out/out.txt"
 
 void ltrim(char *str, const char *seps);
 void rtrim(char *str, const char *seps);
 void trim(char *str, const char *seps);
 void chop(char *str);
-void strChunk(char *output, char *str);
+char *strChunk(char *str);
+void getNRandoms(int n, int max, int *r);
 
 struct Person
 {
     char *name;
     char *lastName;
     char *localCode;
+    int age;
     char gen;  //own gender
     char igen; // interest gender
 };
@@ -29,6 +32,8 @@ int main()
 {
     FILE *fCities = fopen(PATH_CITIES, "r");
     FILE *fPeople = fopen(PATH_PEOPLE, "r");
+    FILE *fOut = fopen(PATH_OUT, "w");
+    
     char GENDERS[] = {'M', 'F'};
     char IGENDERS[] = {'F', 'M', 'A', 'N'};
 
@@ -57,39 +62,57 @@ int main()
 
     struct Person people[MAX_PEOPLE];
     int lengthPeople = 0;
-    char *localCode = "", *gen = "", *igen = "";
-    int pre_localCode = 0, pre_gen = 0, pre_igen = 0;
+    int pre_localCode = 0, pre_gen = 0, pre_igen = 0, pre_age = 0;
     while (fgets(line, MAX_STR_LENGTH - 1, fPeople) != NULL && lengthPeople < MAX_PEOPLE)
     {
         trim(line, NULL);
-        strChunk(people[lengthPeople].name, line);
-        strChunk(people[lengthPeople].lastName, line);
         
-        trim(people[lengthPeople].name, NULL);
-        trim(people[lengthPeople].lastName, NULL);
+        char *name = strChunk(line);
+        char *lastName = strChunk(line);
+		char *localCode = strChunk(line);
+		char *age = strChunk(line);
+        char *gen = strChunk(line);
+        char *igen =strChunk(line);
+
+        trim(name, NULL);
+        trim(lastName, NULL);
 		
-		strChunk(localCode, line);
-        strChunk(gen, line);
-        strChunk(igen, line);
+		//printf("name: %s, lastName: %s, localCode: %s, age: %s,gen: %s, igen: %s\n", name, lastName, localCode, age,gen, igen);
+
 
         sscanf(localCode, "%d", &pre_localCode);
+        sscanf(age, "%d", &pre_age);
         sscanf(gen, "%d", &pre_gen);
         sscanf(igen, "%d", &pre_igen);
+        
+        //printf("name: %s, lastName: %s, localCode: %s, gen: %c, igen: %c\n", name, lastName, cities[pre_localCode - 1], GENDERS[pre_gen - 1], IGENDERS[pre_igen - 1]);
 
+		
+        people[lengthPeople].name = name;
+        people[lengthPeople].lastName = lastName;
         people[lengthPeople].localCode = cities[pre_localCode - 1];
+        people[lengthPeople].age = pre_age;
         people[lengthPeople].gen = GENDERS[pre_gen - 1];
         people[lengthPeople].igen = IGENDERS[pre_igen - 1];
-
+		
         lengthPeople += 1;
     }
-
-    forn(i, lengthPeople)
-    {
-        struct Person x = people[i];
-        printf("Name: %s, lastName: %s, city: %s, gender: %c, igender: %c\n", x.name, x.lastName, x.localCode, x.gen, x.igen);
-    }
+	int n;
+	do{
+		printf("Ingrese la cantidad de personas que desea obtener (max: %d): ", lengthPeople);
+		scanf("%d", &n);
+	}while(n > lengthPeople);
+	
+	int r[n];
+	getNRandoms(n, lengthPeople, r);
+        
+    forn(i, n){
+        fprintf(fOut, "Name: %s, lastName: %s, city: %s, age: %d,gender: %c, igender: %c\n",
+        people[r[i]].name, people[r[i]].lastName, people[r[i]].localCode, people[r[i]].age, people[r[i]].gen, people[r[i]].igen);
+	}
     fclose(fCities);
     fclose(fPeople);
+    fclose(fOut);
     return 0;
 }
 
@@ -149,11 +172,28 @@ void chop(char *str)
     memmove(str, str + i + 1, len - i + 1);
 }
 
-void strChunk(char *output, char *str){
-	int i = 0;
+char *strChunk(char *str){
+	int n = 0;
     int len = strlen(str);
-    while (str[i] != ',' && i < len)
-        i++;
-    strncpy(output, str, i);
+    while (str[n] != ',' && n < len)
+        n++;
+    char *output = (char *)malloc(sizeof(char)*(n+1));
+    //strncpy(output, str, i); Unespected behavior
+    forn(i,n) output[i] = str[i];
+    output[n] = '\0';
     chop(str);
+    return output;
+}
+
+void getNRandoms(int n, int max, int *r){
+	int p[n];
+	int new;
+	forn(i,n) p[i] = 0; 
+	forn(i,n){
+		do{
+			new = rand() % max;
+		}while(p[new] == 1);
+		p[new] = 1;
+		r[i] = new;
+	}
 }
