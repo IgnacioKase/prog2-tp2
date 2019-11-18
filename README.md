2ca04dc86507f73672013b94920026a97ee4a8a1
Ignacio Kasevich <ignacio@kasevich.com.ar>
2019-11-17 21:46:47 -0300

    [Final] commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # prog2-tp2
+
 TPN°2 C
+
+# Compilar
+
+Para compilar el programa,
+
+\$ gcc \_\_main.c
+
+# Ejecutar
+
+\$ fileName arg1 arg2 arg3
+
+# Argumentos
+
+arg1 = Path al archivo de ciudades
+arg2 = Path al archivo de personas
+arg3 = Path al archivo de salida
+
+# Utilidad
+
+Este programa toma 3 archivos:
+
+- Archivo de ciudades
+- Archivo de personas
+- Archivo de salida
+
+Luego pide un numero de personas deseada, digamos x
+y en el archivo de salida escribe x personas tomadas
+aleatoriamente del archivo de personas, con formato
+
+NOMBRE,APELLIDO,CIUDAD,EDAD,GENERO,GENERO_DE_INTERES
+
+La ciudad será el nombre que le corresponda al código
+escrito en el archivo de personas. Al igual que el
+género y el género de interes.
+
+Ejemplos de los archivos de entrada se pueden encontrar
+en la carpeta db
+
+# Boceto de funcionamiento
+
+Recibo la cantidad de personas pedidas, llamese p
+
+Tomo con una chance del 40% algunas personas, supongamos, m personas.
+Cuento la cantidad de personas totales del archivo, supongamos n personas.
+
+Ahora si m < p,
+genero p-m numeros aleatorios, que no esten en r,
+siendo r el conjunto de personas seleccionadas anteriormente.
+
+Una vez generado el conjunto nuevo de numeros,
+recorro una vez mas el archivo para conseguir las personas faltantes.
+
+A la vez que voy cargando las personas, busco la ciudad de codigo mas alto
+y lo guardo en @max_city, estas serán el total de ciudades que necesitaré entrar.
+
+Leo las @max_city ciudades.
+
+Escribo los archivos de salida
+
+#######
+Este approach sirve para ~10^4 ciudades y ~4\*10^4 personas
--- __main.c
+++ __main.c
@@ -2,26 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define forr(i, a, b) for (int i = a; i < b; i+=1)
+/*
+    Definiciones para facilidades a la hora
+    de programar
+*/
+#define forr(i, a, b) for (int i = a; i < b; i += 1)
 #define forn(i, n) forr(i, 0, n)
-#define MAX_CITIES 2134
-#define MAX_PEOPLE 100000
 #define MAX_STR_LENGTH 200
-#define PATH_CITIES "db/codigoLocalidades.txt"
-#define PATH_PEOPLE "db/personas.txt"
-#define PATH_OUT "out/out.txt"
 typedef unsigned long ul;
 
-void ltrim(char *str, const char *seps);
-void rtrim(char *str, const char *seps);
-void trim(char *str, const char *seps);
-void chop(char *str);
-char *strChunk(char *str);
-void getNRandoms(ul n, ul max, ul r[]);
-void swap(ul* a, ul* b);
-int partition (ul arr[], int low, int high);
-void quickSort(ul arr[], int low, int high);
-
+/*
+    Estructura utilizada para representar
+    a las personas en el problema
+*/
 struct Person
 {
     char *name;
@@ -32,108 +25,198 @@ struct Person
     char igen; // interest gender
 };
 
-int main()
+void ltrim(char *str, const char *seps);
+void rtrim(char *str, const char *seps);
+void trim(char *str, const char *seps);
+void chop(char *str);
+char *strChunk(char *str);
+void getNRandoms(ul max, ul a[], ul r[], ul sizeA, ul sizeR);
+void swap(ul *a, ul *b);
+int partition(ul arr[], int low, int high);
+void quickSort(ul arr[], int low, int high);
+void loadPerson(char *line, struct Person *p, char GENDERS[], char IGENDERS[]);
+int checkFiles(FILE *files[], int n);
+
+/*
+    Toma 3 archivos:
+        1 - Archivo de ciudades
+        2 - Archivo de personas
+        3 - Archivo de salida
+
+    Luego pide un numero de personas deseada, digamos x
+    y en el archivo de salida escribe x personas tomadas
+    aleatoriamente del archivo de personas, con formato
+
+    NOMBRE,APELLIDO,CIUDAD,EDAD,GENERO,GENERO_DE_INTERES
+
+    La ciudad será el nombre que le corresponda al código
+    escrito en el archivo de personas. Al igual que el
+    género y el género de interes.
+
+    Ejemplos de los archivos de entrada se pueden encontrar
+    en la carpeta db
+*/
+int main(int argc, char *argv[])
 {
+    //Checkeamos recibir los parametros correctamente
+    if (argc != 4)
+    {
+        printf("---------------------------------------\n\n");
+        printf("Error, ingrese el path de los archivos:\n\n");
+        printf("1 - Archivo de ciudades\n");
+        printf("2 - Archivo de personas\n");
+        printf("3 - Archivo de salida\n\n");
+        printf("Ejemplo:\n\n");
+        printf("$ gcc __main.c\n");
+        printf("$ fileName db/codigoLocalidades.txt db/personas.txt out/out.txt\n\n");
+        printf("---------------------------------------\n");
+        return -1;
+    }
+
+    //Abrimos los archivos correspondientes
+    char *PATH_CITIES = argv[1];
+    char *PATH_PEOPLE = argv[2];
+    char *PATH_OUT = argv[3];
+
     FILE *fCities = fopen(PATH_CITIES, "r");
     FILE *fPeople = fopen(PATH_PEOPLE, "r");
     FILE *fOut = fopen(PATH_OUT, "w+");
-    
+    FILE *files[] = {fCities, fPeople, fOut};
+
+    //Si no se puede abrir algún archivo, cortamos la ejecución
+    if (checkFiles(files, (int)(sizeof(files) / sizeof(files[0]))) == -1)
+        return -1;
+    ///////////////////////////////////////////////////
+
+    //Arreglos que nos serviran para covertir los codigos de genero
+    //al caracter correspondiente
     char GENDERS[] = {'M', 'F'};
     char IGENDERS[] = {'F', 'M', 'A', 'N'};
+    ///////////////////////////////////////////////////
 
-    if (fCities == NULL)
+    //Obtenemos la cantidad de personas
+    ul requestedP; // unsigned long <=> ul
+    do
     {
-        perror(strcat("Error opening: ", PATH_CITIES));
-        return -1;
+        printf("Ingrese la cantidad de personas que desea obtener: ");
+        scanf("%lu", &requestedP);
+    } while (requestedP < 1);
+    ///////////////////////////////////////////////////
+
+    ul *selected = (ul *)malloc(sizeof(ul) * requestedP); //Array para saber que personas fueron elegidas
+    forn(i, requestedP) selected[i] = 0;
+
+    struct Person people[requestedP];
+    ul max_city = 0;
+    ul lengthPeople = 0; //Tendrá el tamaño 'util' es decir donde hay personas ya seteadas de @people
+    ul currentLine = 0;
+    char line[MAX_STR_LENGTH]; //buffer para lecutura
+
+    //Hacemos la primer entrada de las personas, tomando de forma aleatoria
+    while (fgets(line, MAX_STR_LENGTH - 1, fPeople) != NULL && lengthPeople < requestedP)
+    {
+        currentLine++;
+        if (rand() % 100 > 40) //" '40%' de probabilidad de entrar";
+            continue;
+
+        loadPerson(line, &people[lengthPeople], GENDERS, IGENDERS);
+
+        //guardaremos cual es la ciudad con indice mas alto, para luego
+        //leer el archivo de ciudades hasta este punto
+        max_city = people[lengthPeople].localCode > max_city ? people[lengthPeople].localCode : max_city;
+
+        selected[lengthPeople] = currentLine;
+        lengthPeople += 1;
+    }
+    ///////////////////////////////////////////////////
+
+    long sizeFile = currentLine; //al recorrer una vez, ya sabemos el tamaño del archivo
+    /*
+        A este punto, puede que hayamos obtenido todas las personas
+        pero también es posible que no, lo sabremos si @differ es positivo
+    */
+    long differ = (long)requestedP - (long)lengthPeople;
+
+    if (differ > 0)
+    {
+        /*
+            En caso de faltar personas, ahora que sabemos la
+            cantidad de personas que tiene el archivo y la cantidad
+            de personas que faltan, generaremos un array de indices
+            únicos (de forma aleatoria), sobre las personas que no 
+            hayan sido seleccionadas,de tamaño exactamente |differ| 
+            (las que faltan) para que al recorrer el archivo, 
+            tomemos exactamente esta cantidadde personas. 
+            De esta forma podemos, recorriendo solo una vez mas,
+            tomar de forma aleatoria todas las personas requeridas
+        */
+        ul *randoms = (ul *)malloc(sizeof(ul) * differ);
+        quickSort(selected, 0, requestedP - 1);
+        getNRandoms(sizeFile, randoms, selected, differ, requestedP);
+
+        //volvemos al inicio del archivo
+        fclose(fPeople);
+        fPeople = fopen(PATH_PEOPLE, "r");
+
+        currentLine = 0;
+        ul index = 0;
+
+        //Ahora obtenemos las personas faltantes
+        while (fgets(line, MAX_STR_LENGTH - 1, fPeople) != NULL && lengthPeople < requestedP)
+        {
+            currentLine++;
+            if (randoms[index] != currentLine)
+                continue;
+
+            loadPerson(line, &people[lengthPeople], GENDERS, IGENDERS);
+            max_city = people[lengthPeople].localCode > max_city ? people[lengthPeople].localCode : max_city;
+
+            lengthPeople++;
+            index++;
+        }
+        ///////////////////////////////////////////////////
     }
 
-    if (fPeople == NULL)
-    {
-        perror(strcat("Error opening: ", PATH_PEOPLE));
-        return -1;
-    }
-
-	if (fOut == NULL)
-    {
-        perror(strcat("Error opening: ", PATH_OUT));
-        return -1;
-    }
-    
-    ul n; // unsigned long <=> ul
-	do{
-		printf("Ingrese la cantidad de personas que desea obtener (max: %d): ", MAX_PEOPLE);
-		scanf("%lu", &n);
-	}while(n > MAX_PEOPLE);
-	
-	ul *r = (ul *)malloc(sizeof(ul)*n);
-	getNRandoms(n, MAX_PEOPLE, r);
-	
-    char cities[n][MAX_STR_LENGTH];
+    //coregimos el indice de las ciudades, para el tamaño del array
+    max_city++;
+    char cities[max_city][MAX_STR_LENGTH];
     unsigned long lengthCities = 0;
-    char line[MAX_STR_LENGTH];
-    
-    while (fgets(line, MAX_STR_LENGTH - 1, fCities) != NULL && lengthCities < n)
-    {	
+
+    //Obtenemos las ciudades hasta @max_city y las guardamos en @cities
+    while (fgets(line, MAX_STR_LENGTH - 1, fCities) != NULL && lengthCities < max_city)
+    {
         trim(line, NULL);
         chop(line);
         strcpy(cities[lengthCities], line);
         lengthCities += 1;
     }
+    ///////////////////////////////////////////////////
 
-    struct Person people[MAX_PEOPLE];
-    int lengthPeople = 0;
-    int pre_localCode = 0, pre_gen = 0, pre_igen = 0, pre_age = 0, currentLine = 0;
-    while (fgets(line, MAX_STR_LENGTH - 1, fPeople) != NULL && lengthPeople < MAX_PEOPLE)
+    //Escribimos la salida en el archivo
+    forn(i, requestedP)
     {
-		currentLine += 1;
-		if((currentLine-1) != r[lengthPeople])
-			continue;
-        trim(line, NULL);
-        
-        char *name = strChunk(line);
-        char *lastName = strChunk(line);
-		char *localCode = strChunk(line);
-		char *age = strChunk(line);
-        char *gen = strChunk(line);
-        char *igen =strChunk(line);
-
-        trim(name, NULL);
-        trim(lastName, NULL);
-		
-		//printf("name: %s, lastName: %s, localCode: %s, age: %s,gen: %s, igen: %s\n", name, lastName, localCode, age,gen, igen);
-
-
-        sscanf(localCode, "%d", &pre_localCode);
-        sscanf(age, "%d", &pre_age);
-        sscanf(gen, "%d", &pre_gen);
-        sscanf(igen, "%d", &pre_igen);
-        
-        //printf("name: %s, lastName: %s, localCode: %s, gen: %c, igen: %c\n", name, lastName, cities[pre_localCode - 1], GENDERS[pre_gen - 1], IGENDERS[pre_igen - 1]);
-
-		printf("%d", pre_localCode);
-        people[lengthPeople].name = name;
-        people[lengthPeople].lastName = lastName;
-        people[lengthPeople].localCode = pre_localCode - 1;
-        people[lengthPeople].age = pre_age;
-        people[lengthPeople].gen = GENDERS[pre_gen - 1];
-        people[lengthPeople].igen = IGENDERS[pre_igen - 1];
-		
-        lengthPeople += 1;
+        fprintf(fOut, "%s,%s,%s,%d,%c,%c\n",
+                people[i].name, people[i].lastName, cities[people[i].localCode], people[i].age, people[i].gen, people[i].igen);
     }
-	 
-    forn(i, n){
-		printf("%s", cities[people[r[i]].localCode]);
-        fprintf(fOut, "Name: %s, lastName: %s, city: %s, age: %d,gender: %c, igender: %c\n",
-        people[r[i]].name, people[r[i]].lastName, cities[people[r[i]].localCode], people[r[i]].age, people[r[i]].gen, people[r[i]].igen);
-	}
+    ///////////////////////////////////////////////////
+
+    //Cerramos los archivos
     fclose(fCities);
     fclose(fPeople);
     fclose(fOut);
+    ///////////////////////////////////////////////////
     return 0;
 }
 
 /************************ STRING UTILITIES ****************************/
 
+/*
+    Toma una string(array de char) y elimina
+    los caracteres: en @seps o los siguientes si
+    seps es NULL:
+    \t\n\v\f\r (incluido espacios en blancos)
+    del lado izquierdo de la string
+*/
 void ltrim(char *str, const char *seps)
 {
     size_t totrim;
@@ -156,6 +239,13 @@ void ltrim(char *str, const char *seps)
     }
 }
 
+/*
+    Toma una string(array de char) y elimina
+    los caracteres: en @seps o los siguientes si
+    seps es NULL:
+    \t\n\v\f\r (incluido espacios en blancos)
+    del lado derecho de la string
+*/
 void rtrim(char *str, const char *seps)
 {
     int i;
@@ -171,6 +261,13 @@ void rtrim(char *str, const char *seps)
     }
 }
 
+/*
+    Toma una string(array de char) y elimina
+    los caracteres: en @seps o los siguientes si
+    seps es NULL:
+    \t\n\v\f\r (incluido espacios en blancos)
+    del lado derecho e izquierdo de la string
+*/
 void trim(char *str, const char *seps)
 {
     rtrim(str, seps);
@@ -179,6 +276,13 @@ void trim(char *str, const char *seps)
 
 /************************* AUX. FUNCTIONS *****************************/
 
+/*
+    Toma una string(array de char) y elimina
+    todos los caracteres hasta la primer coma
+    encontrada (de izquierda a derecha) con
+    coma incluida, o toda la string en caso de
+    no tener una coma
+*/
 void chop(char *str)
 {
     int i = 0;
@@ -188,84 +292,182 @@ void chop(char *str)
     memmove(str, str + i + 1, len - i + 1);
 }
 
-char *strChunk(char *str){
-	int n = 0;
+/*
+    Toma una string(array de char) y devuelve
+    una substring hasta la primer coma encontrada
+    (o toda la string en caso de no tener una)
+    y además elimina esta substring (con la coma)
+    de la string @str
+*/
+char *strChunk(char *str)
+{
+    int n = 0;
     int len = strlen(str);
     while (str[n] != ',' && n < len)
         n++;
-    char *output = (char *)malloc(sizeof(char)*(n+1));
+    char *output = (char *)malloc(sizeof(char) * (n + 1));
     //strncpy(output, str, i); Unespected behavior
-    forn(i,n) output[i] = str[i];
+    forn(i, n) output[i] = str[i];
     output[n] = '\0';
     chop(str);
     return output;
 }
 
-void swap(ul* a, ul* b)  
-{  
-    ul t = *a;  
-    *a = *b;  
-    *b = t;  
-}  
-  
-int partition (ul arr[], int low, int high)  
-{  
-    ul pivot = arr[high];
-    ul i = (low - 1); 
-  
-    for (ul j = low; j <= high - 1; j++)  
-    {  
-        if (arr[j] < pivot)  
-        {  
-            i++;
-            swap(&arr[i], &arr[j]);  
-        }  
-    }  
-    swap(&arr[i + 1], &arr[high]);  
-    return (i + 1);  
-}  
-
-void quickSort(ul arr[], int low, int high)  
-{  
-    if (low < high)  
-    {  
-        ul pi = partition(arr, low, high);  
-        quickSort(arr, low, pi - 1);  
-        quickSort(arr, pi + 1, high);  
-    }  
-}  
-  
-int search(ul a[], ul n, ul data) {
-   forn(i,n)
-	   if(a[i] == data)
-		return 1;
-	return -1;
+/*
+    Toma dos direcciones de memoria de unsigned long
+    e intercambia los valores que se encuentran en ellas
+*/
+void swap(ul *a, ul *b)
+{
+    ul t = *a;
+    *a = *b;
+    *b = t;
 }
 
 /*
-void getNRandoms(ul n, ul max, ul r[]){
-	ul new;
-	forn(i,n) r[i] = 0;
-	forn(i,n){
-		do{
-			new = rand() % max;
-		}while(search(r,n,new) != -1);	
-		r[i] = new;
-	}
-	quickSort(r,0,n-1);
-}*/
+    Toma el ultimo elemento como pivote, lo posiciona 
+    en el lugar correcto y ubica todos los elementos
+    mas chicos a la izquierda y todos los mas grandes
+    a la derecha del pivote. Devuelve la posición del
+    pivote
+*/
+int partition(ul arr[], int low, int high)
+{
+    ul pivot = arr[high];
+    ul i = (low - 1);
 
-void getNRandoms(ul n, ul max, ul r[]){
-	ul new;
-	char p[max];
-	forn(i,n) r[i] = 0;
-	forn(i,max) p[i] = 0;
-	forn(i,n){
-		do{
-			new = rand() % max;
-		}while(search(r,n,new) != -1);	
-		r[i] = new;
-	}
-	quickSort(r,0,n-1);
+    for (ul j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
 }
 
+/*
+    Toma un array de ul, el incio (@low) y el fin (@high)
+    y lo ordena de menor a mayor
+*/
+void quickSort(ul arr[], int low, int high)
+{
+    if (low < high)
+    {
+        ul pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+/*
+    Toma un array, su tamaño (@n) y un valor a buscar (@data)
+    y devuelve el indice donde se encuentra dicho valor
+    en caso de no estar en el array, devuelve -1
+*/
+int binarySearch(ul a[], ul n, ul data)
+{
+    ul lowerBound = 0;
+    ul upperBound = n - 1;
+    ul midPoint = -1;
+    ul index = -1;
+
+    while (lowerBound <= upperBound)
+    {
+        midPoint = lowerBound + (upperBound - lowerBound) / 2;
+        if (a[midPoint] == data)
+        {
+            index = midPoint;
+            break;
+        }
+        else
+        {
+            if (a[midPoint] < data)
+                lowerBound = midPoint + 1;
+            else
+                upperBound = midPoint - 1;
+        }
+    }
+    return index;
+}
+
+/*
+    Escribe en el array @a y en el array @r, numeros aleatorios
+    que no se encuentren previamente en @r,
+    los numeros aleatorios van de 1 a @max
+*/
+void getNRandoms(ul max, ul a[], ul r[], ul sizeA, ul sizeR)
+{
+    ul new;
+
+    forn(i, sizeA) a[i] = 0;
+    forn(i, sizeA)
+    {
+        do
+        {
+            new = rand() % max + 1;
+        } while (binarySearch(r, sizeR, new) != -1);
+        a[i] = new;
+        r[0] = new;
+        quickSort(r, 0, sizeR);
+    }
+    quickSort(a, 0, sizeA - 1);
+}
+
+/*
+    Recibe una linea del archivo de entrada
+    y carga los datos en @p, siendo los datos
+    Nombre
+    Apellido
+    Ciudad (código numerico)
+    Edad
+    Género (char correspondiente)
+    Género de interés (char correspondiente)
+*/
+void loadPerson(char *line, struct Person *p, char GENDERS[], char IGENDERS[])
+{
+    ul pre_localCode = 0, pre_gen = 0, pre_igen = 0, pre_age = 0;
+    trim(line, NULL);
+
+    char *name = strChunk(line);
+    char *lastName = strChunk(line);
+    char *localCode = strChunk(line);
+    char *age = strChunk(line);
+    char *gen = strChunk(line);
+    char *igen = strChunk(line);
+
+    trim(name, NULL);
+    trim(lastName, NULL);
+
+    sscanf(localCode, "%lu", &pre_localCode);
+    sscanf(age, "%lu", &pre_age);
+    sscanf(gen, "%lu", &pre_gen);
+    sscanf(igen, "%lu", &pre_igen);
+
+    p->name = name;
+    p->lastName = lastName;
+    p->localCode = pre_localCode - 1;
+    p->age = pre_age;
+    p->gen = GENDERS[pre_gen - 1];
+    p->igen = IGENDERS[pre_igen - 1];
+}
+
+/*
+    Recibe un array de FILE* y chequea que no
+    sean NULL, si alguno lo es, devuelve -1
+    en caso contrario, devuelve 0
+*/
+int checkFiles(FILE *files[], int n)
+{
+    forn(i, n)
+    {
+        if (files[i] == NULL)
+        {
+            perror("Error opening: a file.");
+            return -1;
+        }
+    }
+    return 0;
+}
